140bf4b0d6ae778a029cbe529b0a655c3072c77e
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-06-30 19:47:44 +0200

    Update Prisustvo.java

--- src/main/java/com/edukatorplus/model/Prisustvo.java
+++ src/main/java/com/edukatorplus/model/Prisustvo.java
@@ -1,11 +1,6 @@
 package com.edukatorplus.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 
 @Entity
 public class Prisustvo {
@@ -13,42 +8,23 @@ public class Prisustvo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long polaznikId;
     private Long radionicaId;
 
     @Enumerated(EnumType.STRING)
-    private StatusPrisustva status;  // Koristimo Enum za status prisustva
+    private StatusPrisustva status;
 
     // Getteri i setteri
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getPolaznikId() { return polaznikId; }
+    public void setPolaznikId(Long polaznikId) { this.polaznikId = polaznikId; }
 
-    public Long getPolaznikId() {
-        return polaznikId;
-    }
+    public Long getRadionicaId() { return radionicaId; }
+    public void setRadionicaId(Long radionicaId) { this.radionicaId = radionicaId; }
 
-    public void setPolaznikId(Long polaznikId) {
-        this.polaznikId = polaznikId;
-    }
-
-    public Long getRadionicaId() {
-        return radionicaId;
-    }
-
-    public void setRadionicaId(Long radionicaId) {
-        this.radionicaId = radionicaId;
-    }
-
-    public StatusPrisustva getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusPrisustva status) {
-        this.status = status;
-    }
+    public StatusPrisustva getStatus() { return status; }
+    public void setStatus(StatusPrisustva status) { this.status = status; }
 }
