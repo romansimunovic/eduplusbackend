01ddebb09400bcf8da873a40759d296865cea2c2
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-06-30 19:06:39 +0200

    Update Start.java

--- src/main/java/com/edukatorplus/eduplusbackend/Start.java
+++ src/main/java/com/edukatorplus/eduplusbackend/Start.java
@@ -2,8 +2,10 @@ package com.edukatorplus.eduplusbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.edukatorplus") 
+@SpringBootApplication(scanBasePackages = "com.edukatorplus")
+@EnableJpaRepositories(basePackages = "com.edukatorplus.repository") 
 public class Start {
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
