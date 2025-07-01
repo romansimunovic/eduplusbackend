9b87fe542cd5bd7e880ddf5f13c9d2efa6380e37
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-07-01 17:19:34 +0200

    Update Radionica.java

--- src/main/java/com/edukatorplus/model/Radionica.java
+++ src/main/java/com/edukatorplus/model/Radionica.java
@@ -1,6 +1,6 @@
 package com.edukatorplus.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -12,41 +12,20 @@ public class Radionica {
 
     private String naziv;
 
-    @Column(nullable = false)
     private LocalDate datum;
 
-    // Prazan konstruktor
-    public Radionica() {
-    }
+    public Radionica() {}
 
-    // Konstruktor s parametrima
     public Radionica(String naziv, LocalDate datum) {
         this.naziv = naziv;
         this.datum = datum;
     }
 
-    // Getteri i setteri
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public String getNaziv() { return naziv; }
+    public LocalDate getDatum() { return datum; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNaziv() {
-        return naziv;
-    }
-
-    public void setNaziv(String naziv) {
-        this.naziv = naziv;
-    }
-
-    public LocalDate getDatum() {
-        return datum;
-    }
-
-    public void setDatum(LocalDate datum) {
-        this.datum = datum;
-    }
+    public void setId(Long id) { this.id = id; }
+    public void setNaziv(String naziv) { this.naziv = naziv; }
+    public void setDatum(LocalDate datum) { this.datum = datum; }
 }
