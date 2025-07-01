00a2540efdf5a37aa2eb13b7dc64647abd0caf73
Roman Šimunović <67165057+romanuspopulsque@users.noreply.github.com>
2025-07-01 15:54:50 +0200

    Update Start.java

--- src/main/java/com/edukatorplus/eduplusbackend/Start.java
+++ src/main/java/com/edukatorplus/eduplusbackend/Start.java
@@ -1,15 +1,28 @@
 package com.edukatorplus.eduplusbackend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.edukatorplus")
 @EnableJpaRepositories(basePackages = "com.edukatorplus.repository")
-@EntityScan(basePackages = "com.edukatorplus.model")  // <--- OVO JE KLJUČNO
+@EntityScan(basePackages = "com.edukatorplus.model")
 public class Start {
+
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
+    }
+
+    // Registracija modula za LocalDate
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
     }
 }
