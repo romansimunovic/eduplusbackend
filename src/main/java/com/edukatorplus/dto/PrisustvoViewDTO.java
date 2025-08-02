ea5a7278b4f950134d14363a899d1fb93234cd88
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-02 20:19:16 +0200

    Update PrisustvoViewDTO.java

--- src/main/java/com/edukatorplus/dto/PrisustvoViewDTO.java
+++ src/main/java/com/edukatorplus/dto/PrisustvoViewDTO.java
@@ -1,3 +1,9 @@
 package com.edukatorplus.dto;
 
-public record PrisustvoViewDTO(Long id, String polaznikImePrezime, String radionicaNaziv, String status) {}
+public record PrisustvoViewDTO(
+    Long id,
+    String polaznikImePrezime,
+    String radionicaNaziv,
+    String status,
+    String spol
+) {}
