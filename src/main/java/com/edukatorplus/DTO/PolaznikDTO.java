4d399dbb208314e248b18ca4f021ada84b147699
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-02 19:05:08 +0200

    Update PolaznikDTO.java

--- src/main/java/com/edukatorplus/DTO/PolaznikDTO.java
+++ src/main/java/com/edukatorplus/DTO/PolaznikDTO.java
@@ -1,4 +1,2 @@
-package com.edukatorplus.dto;
-
-public record PolaznikDTO(Long id, String ime, String prezime, String email, Integer godinaRodenja) {
-}
+public record PolaznikDTO(Long id, String ime, String prezime, String email, Integer godinaRodenja,
+                          String spol, String telefon, String grad, String status) {}
