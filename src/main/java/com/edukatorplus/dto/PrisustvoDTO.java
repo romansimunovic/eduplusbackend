089d96f4274ec48dcabe0b0432bcec9296d64a4c
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-02 20:31:50 +0200

    Create PrisustvoDTO.java

---
+++ src/main/java/com/edukatorplus/dto/PrisustvoDTO.java
@@ -0,0 +1,10 @@
+package com.edukatorplus.dto;
+
+import com.edukatorplus.model.StatusPrisustva;
+
+public record PrisustvoDTO(
+        Long id,
+        Long polaznikId,
+        Long radionicaId,
+        StatusPrisustva status
+) {}
