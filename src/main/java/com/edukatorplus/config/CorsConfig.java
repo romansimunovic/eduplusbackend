a54ae9e719330ac54be893cf6ce2ae357471d19e
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-14 12:40:49 +0200

    Create CorsConfig.java

---
+++ src/main/java/com/edukatorplus/config/CorsConfig.java
@@ -0,0 +1,27 @@
+// npr. com.edukatorplus.config.CorsConfig
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        String frontendProd = "https://eduplusfrontend.onrender.com";
+        String frontendLocal = "http://localhost:3000";
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendProd, frontendLocal)
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
