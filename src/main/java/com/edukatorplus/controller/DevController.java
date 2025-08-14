01cc62d6ab21c947cc73c643932f57343b81f057
Roman Šimunović <67165057+romansimunovic@users.noreply.github.com>
2025-08-14 10:39:23 +0200

    Update DevController.java

--- src/main/java/com/edukatorplus/controller/DevController.java
+++ src/main/java/com/edukatorplus/controller/DevController.java
@@ -1,19 +1,17 @@
 package com.edukatorplus.controller;
 
 import com.edukatorplus.seeder.DataSeeder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Profile("dev")  // ⬅⬅⬅ samo u dev profilu postoji ovaj kontroler
+@Profile("dev") // ⬅️ podiže se samo u dev profilu
 @RestController
 @RequestMapping("/api/dev")
 public class DevController {
 
     private final DataSeeder dataSeeder;
 
-    @Autowired
     public DevController(DataSeeder dataSeeder) {
         this.dataSeeder = dataSeeder;
     }
